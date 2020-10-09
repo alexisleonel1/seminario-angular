007f7a00c354fbcb295cd9d37bff2ad458e8258f
alexisleonel1 <alexisleogalvan12@gmail.com>
2020-10-09 13:18:58 -0300

    se añadio en menu y el segundo componete tablero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-body',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'apiarios';
+  title = 'Apiarios el caserito';
 }
